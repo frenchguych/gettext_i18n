8226292c29e0e1d34168177fb5ec3fa11a4c9fd0
Yannick Mauray <yannick.mauray@gmail.com>
2021-11-14 21:18:46 +0100

    Fixed : example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 1.0.2
+
+* Fixed example
+
+## 1.0.1
+
+* Fixed packaging issues
+
 ## 1.0.0
 
 * Initial release.
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
-
-import 'app.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:gettext_i18n/gettext_i18n.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const HomePage(),
+      supportedLocales: ['en', 'fr', 'fr_CH'].map((l) => Locale(l)),
+      localizationsDelegates: [
+        GettextLocalizationsDelegate(defaultLanguage: 'en'),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      localeListResolutionCallback: (locales, supportedLocales) {
+        if (locales != null) {
+          for (var locale in locales) {
+            var supportedLocale = supportedLocales.where((element) =>
+                element.languageCode == locale.languageCode &&
+                element.countryCode == locale.countryCode);
+            if (supportedLocale.isNotEmpty) {
+              return supportedLocale.first;
+            }
+            supportedLocale = supportedLocales.where(
+                (element) => element.languageCode == locale.languageCode);
+            if (supportedLocale.isNotEmpty) {
+              return supportedLocale.first;
+            }
+          }
+        }
+        return null;
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(context.t('Hello, world !')),
+    );
+  }
 }
