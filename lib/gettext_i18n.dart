e78377b108c0072a434b1dbeac4d0e6607f8f853
Yannick Mauray <yannick.mauray@gmail.com>
2024-07-01 20:19:30 +0200

    feat: export GettextLocalizations

--- lib/gettext_i18n.dart
+++ lib/gettext_i18n.dart
@@ -3,3 +3,4 @@ library gettext_i18n;
 export 'src/context_ext.dart' show ContextExt;
 export 'src/gettext_localizations_delegate.dart'
     show GettextLocalizationsDelegate;
+export 'src/gettext_localizations.dart' show GettextLocalizations;
